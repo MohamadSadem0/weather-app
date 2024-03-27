fb9fee0757e6b99b451e6b25f298245eaab0ff1a
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-27 17:29:52 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-const App=()=> {
+import Card from "./Components/card";
+import "./App.css";
+import RainCouldIcon from "./assets/icons8-rain-cloud-48.png";
+const App = () => {
   return (
     <div className="App">
-      
+      <Card date="Today" iconLink={RainCouldIcon} temperature={32}></Card>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const card = ({ iconLink, date, condition, tempeture }) => {
+const card = ({ iconLink, date, condition, temperature }) => {
   return (
     <div className="card">
-      <div> {date}</div>
-      <img className="weather-icon" src={iconLink} alt="" />
-      <div className="Temperture">
-        {tempeture}
-        <span>&#xb0</span>
+      <div className="ft-size-64 center"> {date}</div>
+      <img className="weather-icon center mt-55" src={iconLink} alt="" />
+      <div className="Temperture ft-size-32 mt-55">
+        <span className="Temperture ft-size-32"> Temperature: </span>
+        {temperature}
+        <span>&deg;</span>
       </div>
     </div>
   );

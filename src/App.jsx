6544ae5c010e618efb36d056772ecb11cde879d3
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-03-27 22:41:46 +0200

    fixing background grey

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Components/card";
 import "./App.css";
-import RainCouldIcon from "./assets/icons8-rain-cloud-48.png";
+import RainCloudIcon from "./assets/icons8-rain-cloud-48.png";
+import Background from "./assets/pexels-johannes-plenio-1118873 1.png";
+import axios from "axios";
+
 const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://api.weatherapi.com/v1/forecast.json?key=ab7b00a280f448abba3205026242603&q=beirut&days=3"
+        );
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <Card date="Today" iconLink={RainCouldIcon} temperature={32}></Card>
+      <img src={Background} alt="" />
+      <div>Beirut</div>
+      <div className="bck-grey">
+        <div className="cards">
+          <Card
+            date="Today"
+            iconLink={RainCloudIcon}
+            temperature={32}
+            condition={weatherData?.current?.condition?.text}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 const card = ({ iconLink, date, condition, temperature }) => {
   return (
     <div className="card">
-      <div className="ft-size-64 center"> {date}</div>
+      <div className="ft-size-64 center pt-10"> {date}</div>
       <img className="weather-icon center mt-55" src={iconLink} alt="" />
       <div className="Temperture ft-size-32 mt-55">
         <span className="Temperture ft-size-32"> Temperature: </span>
@@ -16,12 +17,7 @@ const card = ({ iconLink, date, condition, temperature }) => {
 
 export default card;
 
-// &#xb0
 
-// const getdata= async()=>{
-// const reponse= await axios.get("");
-// console.log(reponse)
-// response.map(e){
 
-// }
-// }
+
+
